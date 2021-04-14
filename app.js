a0ea83dda9b1395f3d746581ddf782a6f5f4880b
Mayadah <>
2021-04-14 11:05:28 +0000

    write js

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Welcome to My Fav Book Page');
+
+var userAge = prompt('Enter Your Age :');
+if (userAge <= 10) {
+  alert('sorry you are under age');
+ }else{
+   alert('you can read the book');
+ }
+ 
