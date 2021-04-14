7e5e1fffe899f7bc02037cd33724b0d810382859
Mayadah <>
2021-04-14 11:26:57 +0000

    edit js

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ alert('Welcome to My Fav Book Page');
 var userAge = prompt('Enter Your Age :');
 if (userAge <= 10) {
   alert('sorry you are under age');
-  document.write('sorry read another book');
-    document.write('<img src="https://acegif.com/wp-content/gifs/book-93.gif">');
+  document.write('<h3>' + ' sorry read another book ' + '</h3>');
+  document.write('<img src="https://acegif.com/wp-content/gifs/book-93.gif">');
 
- }else if (userAge >= 11){
-   alert('you can read the book');
-   document.write('enjoy reading the Book');
-     document.write('<img src= "https://media1.giphy.com/media/gLukNCxcQ1dBcRnf3y/giphy.gif">');
+} else if (userAge >= 11) {
+  alert('you can read the book');
+  document.write('<h3>' + 'enjoy reading the Book ' + '</h3>');
 
- }
+  document.write('<img src= "https://media1.giphy.com/media/gLukNCxcQ1dBcRnf3y/giphy.gif">');
+
+}
