0dcef079816668c8e49ea83c5064c18c9e56b15d
Mayadah <>
2021-04-14 11:20:05 +0000

    edit js

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ alert('Welcome to My Fav Book Page');
 var userAge = prompt('Enter Your Age :');
 if (userAge <= 10) {
   alert('sorry you are under age');
- }else{
+  document.write('sorry read another book');
+    document.write('<img src="https://acegif.com/wp-content/gifs/book-93.gif">');
+
+ }else if (userAge >= 11){
    alert('you can read the book');
+   document.write('enjoy reading the Book');
+     document.write('<img src= "https://media1.giphy.com/media/gLukNCxcQ1dBcRnf3y/giphy.gif">');
+
  }
- 
